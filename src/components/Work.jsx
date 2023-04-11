ded20ff44a524ca9b04ab5c84ffb3bc735993f19
williamjrr07 <114874231+williamjrr07@users.noreply.github.com>
2023-04-10 21:29:57 -0400

    modificacion

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -68,6 +68,15 @@ const Work = () => {
                 </button>
               </div>
             </div>
+          </motion.div>
+          {/* images R  */}
+          <motion.div
+            variants={fadeIn('left', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-10'
+          >
             {/* image 2 */}
             <div className=' group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               {/* overlay */}
@@ -98,15 +107,6 @@ const Work = () => {
                 </button>
               </div>
             </div>
-          </motion.div>
-          {/* images R  */}
-          <motion.div
-            variants={fadeIn('left', 0.3)}
-            initial='hidden'
-            whileInView={'show'}
-            viewport={{ once: false, amount: 0.3 }}
-            className='flex-1 flex flex-col gap-y-10'
-          >
             {/* image 3 */}
             <div className=' group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               {/* overlay */}
@@ -130,66 +130,6 @@ const Work = () => {
                   <a
                     className='text-sm'
                     href='https://williamjrrformcss.netlify.app/'
-                    target='_blank'
-                  >
-                    go to page
-                  </a>
-                </button>
-              </div>
-            </div>
-            {/* image 4 */}
-            <div className=' group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-              {/* overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-              {/* img */}
-              <img
-                className='w-full group-hover:scale-125 transition-all duration-500'
-                src={img4}
-                alt=''
-              />
-              {/* pretitle */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-gradient'>UI/UX Design</span>
-              </div>
-              {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3xl text-white'>Pokedex con React</span>
-              </div>
-              <div className='absolute -bottom-full left-12 group-hover:bottom-5 transition-all duration-1000 z-50'>
-                <button className='btn pl-4 pr-4 pb-1'>
-                  <a
-                    className='text-sm'
-                    href='https://stellular-cheesecake-fd67ea.netlify.app/'
-                    target='_blank'
-                  >
-                    go to page
-                  </a>
-                </button>
-              </div>
-            </div>
-            {/* image 5 */}
-            <div className=' group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-              {/* overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-              {/* img */}
-              <img
-                className='w-full group-hover:scale-125 transition-all duration-500'
-                src={img5}
-                alt=''
-              />
-              {/* pretitle */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-gradient'>UI/UX Design</span>
-              </div>
-              {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3xl text-white'>Pokedex con React</span>
-              </div>
-              <div className='absolute -bottom-full left-12 group-hover:bottom-5 transition-all duration-1000 z-50'>
-                <button className='btn pl-4 pr-4 pb-1'>
-                  <a
-                    className='text-sm'
-                    href='https://pokedexjswilliamjrr.netlify.app/'
                     target='_blank'
                   >
                     go to page
